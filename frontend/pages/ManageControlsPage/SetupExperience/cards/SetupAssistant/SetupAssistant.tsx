a1a35a5efc467f63043cab31e2306b9885d2a255
Magnus Jensen <magnus@fleetdm.com>
2026-04-27 15:39:15 -0600

    better types

--- frontend/pages/ManageControlsPage/SetupExperience/cards/SetupAssistant/SetupAssistant.tsx
+++ frontend/pages/ManageControlsPage/SetupExperience/cards/SetupAssistant/SetupAssistant.tsx
@@ -125,12 +125,14 @@ const SetupAssistant = ({
           </p>
           {enrollmentProfileNotFound || !enrollmentProfileData ? (
             <>
-              <SetupAssistantProfileCard
-                profile={
-                  defaultEnrollmentProfileData as IAppleSetupEnrollmentProfileResponse
-                }
-                defaultProfile
-              />
+              {defaultEnrollmentProfileData && (
+                <SetupAssistantProfileCard
+                  profile={
+                    defaultEnrollmentProfileData as IAppleSetupEnrollmentProfileResponse
+                  }
+                  defaultProfile
+                />
+              )}
               <SetupAssistantProfileUploader
                 currentTeamId={currentTeamId}
                 onUpload={onUpload}
